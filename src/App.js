4b30259ab896c06cc40e4747d84b37d59d92e417
Stevi Deter <stevi.deter@gmail.com>
2018-06-24 16:43:10 -0700

    rename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './madslibs-pout.png';
+import madspout from './madslibs-pout.png';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={madspout} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Madslibs</h1>
         </header>
         <p className="App-intro">
