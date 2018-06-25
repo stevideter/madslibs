3cb7d64b651f4b645d32b6154bca4d1f8bc9f512
Stevi Deter <stevi.deter@gmail.com>
2018-06-25 08:04:50 -0700

    add one more

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,33 +3,34 @@ import madspout from "../madslibs-pout.png";
 import "./App.css";
 import Madslibs from "./Madslibs/Madslibs";
 import Fillin from "./Fillin/Fillin";
-import 'typeface-roboto';
+import "typeface-roboto";
 class App extends Component {
     constructor(props) {
         super(props);
-        const replacements = [
-            { partOfSpeech: "noun (plural)", replace: "eyes" },
-            { partOfSpeech: "verb", replace: "see" },
-            { partOfSpeech: "verb", replace: "focus" },
-            { partOfSpeech: "color", replace: "white" },
-        ];
+        // const replacements = [
+        //     { partOfSpeech: "noun (plural)", replace: "eyes" },
+        //     { partOfSpeech: "verb", replace: "see" },
+        //     { partOfSpeech: "verb", replace: "focus" },
+        //     { partOfSpeech: "color", replace: "white" },
+        // ];
         this.state = {
-            replacements,
-            words: Array(replacements.length).fill(""),
+            replacements: [],
+            words: [],
             fillinDone: false,
             madslib: "",
-            showMadslib: false,
+            showMadslib: false
         };
         this.handleWordChange = this.handleWordChange.bind(this);
         this.handleDone = this.handleDone.bind(this);
     }
     handleDone() {
-        this.setState({showMadslib: true});
+        this.setState({ showMadslib: true });
     }
     handleWordChange(i, value) {
         const words = this.state.words.slice();
         words[i] = value;
-        const fillinDone = words.filter(word => word.length > 0).length === words.length;
+        const fillinDone =
+            words.filter(word => word.length > 0).length === words.length;
         this.setState({ words, fillinDone });
     }
     componentDidMount() {
@@ -40,14 +41,18 @@ class App extends Component {
         fetch("/api/madslib")
             .then(res => res.json())
             .then(madslib => {
-                console.log('got ', madslib);
-                this.setState({ madslib: madslib.madslib});
+                console.log("got ", madslib);
+                this.setState({
+                    madslib: madslib.madslib,
+                    replacements: madslib.replacements,
+                    words: Array(madslib.replacements.length).fill("")
+                });
             })
             .catch(err => {
                 console.log(err);
-                this.setState({madslib: "i let you know me, see me"});
+                this.setState({ madslib: "i let you know me, see me" });
             });
-    };    
+    };
     render() {
         const showMadslib = this.state.showMadslib;
         return (
@@ -58,13 +63,13 @@ class App extends Component {
                         Welcome Fannibals to Madslibs!
                     </h1>
                 </header>
-                {showMadslib &&
-                <Madslibs
-                    words={this.state.words}
-                    replacements={this.state.replacements}
-                    madslib={this.state.madslib}
-                />
-                }
+                {showMadslib && (
+                    <Madslibs
+                        words={this.state.words}
+                        replacements={this.state.replacements}
+                        madslib={this.state.madslib}
+                    />
+                )}
                 <Fillin
                     words={this.state.words}
                     handleWordChange={this.handleWordChange}
--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Put all API endpoints under '/api'
-app.get('/api/madslib', (req, res) => {
-    const madslib = `
+app.use(express.static(path.join(__dirname, "client/build")));
+const madslibs = [
+    {
+        madslib: `
     Eyes are distracting. You see too much. You don’t see
     enough. And it’s hard to focus when you’re thinking those
     whites are really white or they must have hepatitis, or is
     that a burst vein? So I try to avoid eyes whenever possible.
-    `;
-  // Return them as json
-  res.json({madslib});
+    `,
+        replacements: [
+            { partOfSpeech: "noun (plural)", replace: "eyes" },
+            { partOfSpeech: "verb", replace: "see" },
+            { partOfSpeech: "verb", replace: "focus" },
+            { partOfSpeech: "color", replace: "white" }
+        ]
+    },
+    {
+        madslib: `
+    What he has is pure empathy. He can assume your
+    point of view, or mine -- and maybe
+    some other points of view that
+    scare him. It’s an uncomfortable
+    gift, Jack. Perception’s a tool
+    that’s pointed on both ends.
+    `,
+        replacements: [
+            { partOfSpeech: "noun", replace: "empathy" },
+            { partOfSpeech: "verb", replace: "assume" },
+            { partOfSpeech: "noun", replace: "point" },
+            { partOfSpeech: "noun", replace: "view" },
+            { partOfSpeech: "adjective", replace: "uncomfortable" },
+            { partOfSpeech: "noun", replace: "Perception" },
+            { partOfSpeech: "adjective", replace: "pointed" }
+        ]
+    }
+];
 
-  console.log(`Sent madslib`);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+// Put all API endpoints under '/api'
+app.get("/api/madslib", (req, res) => {
+    // Return them as json
+    const index = getRandomInt(madslibs.length);
+    const madslib = madslibs[index];
+    res.json(madslib);
+
+    console.log("sending", madslib);
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
