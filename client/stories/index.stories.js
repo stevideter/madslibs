b816270ee6c666e299e030798a51e5fb7bcee5cf
Stevi Deter <stevi.deter@gmail.com>
2018-06-25 23:43:21 -0700

    knobs

--- client/stories/index.stories.js
+++ client/stories/index.stories.js
@@ -3,20 +3,16 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import TextInput from '../src/components/TextInput/TextInput';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-storiesOf('TextInput', module)
-  .add('with label', () => <TextInput label="some label" value="some value">Text Input</TextInput>);
+const stories = storiesOf('Madslibs', module);
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
+stories.add('with label', () =>{ 
+    const label = text('Label', 'My label');
+    const value = text('Value', 'My value');
+    return (<TextInput label={label} value={value}>Text Input</TextInput>);
+});
